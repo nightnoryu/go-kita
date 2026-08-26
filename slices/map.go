7f3de0e4018b49608f83c600a3625bba53d69fe6
nightnoryu <qazwf734@gmail.com>
2026-08-26 09:59:05 +0300

    Add MapErr implementation

--- slices/map.go
+++ slices/map.go
@@ -8,3 +8,16 @@ func Map[T, TResult any](s []T, f func(T) TResult) []TResult {
 	}
 	return result
 }
+
+// MapErr iterates through slice, maps values and stops on error
+func MapErr[T, TResult any](s []T, f func(T) (TResult, error)) ([]TResult, error) {
+	result := make([]TResult, 0, len(s))
+	for _, t := range s {
+		e, err := f(t)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, e)
+	}
+	return result, nil
+}
